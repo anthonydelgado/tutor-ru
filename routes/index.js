e7de6b227d8409f489a773004474fbe6d9ac44c2
Anthony Delgado <me@anthonydelgado.me>
2016-10-23 01:33:41 -0400

    add twilio video

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,15 @@
+
+require('dotenv').load();
+
+var AccessToken = require('twilio').AccessToken;
+var VideoGrant = AccessToken.VideoGrant;
+
 var express = require('express');
 var passport = require('passport');
 var Account = require('../models/account');
+var randomUsername = require('../models/randos');
+
+
 var router = express.Router();
 
 
@@ -29,6 +38,32 @@ router.get('/userlist', function(req, res) {
     // console.log(docs);
   });
 
+});
+
+router.get('/token', function(request, response) {
+  var identity = randomUsername();
+
+  // Create an access token which we will sign and return to the client,
+  // containing the grant we just created
+  var token = new AccessToken(
+      process.env.TWILIO_ACCOUNT_SID,
+      process.env.TWILIO_API_KEY,
+      process.env.TWILIO_API_SECRET
+  );
+
+  // Assign the generated identity to the token
+  token.identity = identity;
+
+  //grant the access token Twilio Video capabilities
+  var grant = new VideoGrant();
+  grant.configurationProfileSid = process.env.TWILIO_CONFIGURATION_SID;
+  token.addGrant(grant);
+
+  // Serialize the token to a JWT string and include it in a JSON response
+  response.send({
+    identity: identity,
+    token: token.toJwt()
+  });
 });
 
 router.post('/register', function(req, res) {
